900a5d29abf214d1e2a4af66b1e2309488ad4719
Miguel Necoechea <necoecheamiguel@gmail.com>
2025-05-07 00:53:14 -0600

    Initial version of coloring based on linguistics.

--- src/scripts/content/linguisticsContents/JapaneseTextColoring.ts
+++ src/scripts/content/linguisticsContents/JapaneseTextColoring.ts
@@ -1,1 +1,21 @@
-// TODO: Future Class to handle the coloring of the text depending on POS NER and other linguistical functions lol.
+import { Token } from "../../models/JapaneseTokens";
+
+export class JapaneseTextColoring {
+    private posColorMap: Record<string, string> = {
+        NOUN: "#1f77b4",
+        VERB: "#d62728",
+        ADJ: "#2ca02c",
+        ADV: "#ff7f0e",
+    };
+
+    public addPOSAnnotations(): void {
+        // select all spans that you previously decorated with data-pos
+        const spans =
+            document.querySelectorAll<HTMLSpanElement>("span[data-pos]");
+        spans.forEach((span) => {
+            const pos = span.dataset.pos!;
+            const color = this.posColorMap[pos] || "#000000";
+            span.style.color = color;
+        });
+    }
+}
